279c29c5516291f1cf4dc9602806443117b7d5e9
M@! <yoat42@gmail.com>
2018-11-28 00:19:00 -0500

    Added and ran build command, edited readme.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # hgmg
 HyperGraph Modeling Grammar
+
+## Install
+
+`> npm i hgmg`
+
+## Use
+
+```js
+import { hmgm, Quadtree, Octree } from "hgmg";
+```
+
+## Build
+
+`> npm run build`
--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,14 @@
-module.exports = function tiny(string) {
-    if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
-    return string.replace(/\s/g, "");
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
 };
+Object.defineProperty(exports, "__esModule", { value: true });
+var pluralize = __importStar(require("pluralize"));
+function getPlural(str) {
+    return pluralize.plural(str);
+}
+exports.getPlural = getPlural;
