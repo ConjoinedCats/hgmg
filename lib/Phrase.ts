819c9275e8427c81803369b20db3820ffd81f509
M@! <yoat42@gmail.com>
2018-11-29 01:32:36 -0500

    Developing QPhrase and QToken models.

--- lib/Phrase.ts
+++ lib/Phrase.ts
@@ -1,17 +1,38 @@
-import { BaseToken, QuadToken, OctToken } from "./Tokens";
+// import { BaseToken } from "./Tokens";
 
 export class QPhrase {
   private _str: string;
-  private _tkns: Array<QuadToken> = [];
+  private _tkns: Array<QToken> = [];
 
   constructor(input: string) {
     this._str = input;
   }
 
-  static parse(input: string): Array<QuadToken> {
-    const retval = new Array<QuadToken>();
-
+  static parse(input: string): Array<QToken> {
+    const retval = new Array<QToken>();
     const chars = [...input];
+    chars.forEach((v: string) => {
+      if (QToken.isValid(v)) {
+        retval.push(new QToken(v));
+      }
+    });
     return retval;
+  }
+}
+
+export class QToken {
+  private _s: string;
+  private static _re = new RegExp("^[0-3]+$");
+
+  constructor(s: string) {
+    this._s = s;
+  }
+
+  get value(): string {
+    return this._s;
+  }
+
+  static isValid(input: string): boolean {
+    return QToken._re.test(input);
   }
 }
