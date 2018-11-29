48a0f7b1855f5ad6446d811d1899269568954f7f
M@! <yoat42@gmail.com>
2018-11-29 01:45:50 -0500

    Updated index.

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,7 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var pluralize = __importStar(require("pluralize"));
+const pluralize = __importStar(require("pluralize"));
 function getPlural(str) {
     return pluralize.plural(str);
 }
