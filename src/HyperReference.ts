daeac7e37926c450a2883eeeb832903dc5de63f2
M@! <yoat42@gmail.com>
2018-12-09 16:58:53 -0500

    Defined HyperReference members.

--- src/HyperReference.ts
+++ src/HyperReference.ts
@@ -1,9 +1,21 @@
 import { RootAddress } from "./RootAddress";
 import { IEquatable } from "./Interfaces";
+import { Address } from "./Address";
 
 export class HyperReference implements IEquatable<HyperReference> {
+  parent: RootAddress;
+  child: RootAddress;
+  link: Address;
+  parentReference: boolean;
+
+  constructor(from: RootAddress, to: RootAddress, loc: Address, isParent: boolean = false) {
+    this.parent = from;
+    this.child = to;
+    this.link = loc;
+    this.parentReference = isParent;
+  }
 
   isEqual(x: HyperReference): boolean {
-    return false;
+    return (this.parent === x.parent && this.child === x.child && this.link === x.link && this.parentReference === x.parentReference);
   }
 }
