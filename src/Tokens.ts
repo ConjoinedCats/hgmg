02e9d4be8aa03000bf391efb66b712972465005e
M@! <yoat42@gmail.com>
2018-12-15 00:23:31 -0500

    WIP fleshing out parse method and data structs.

--- src/Tokens.ts
+++ src/Tokens.ts
@@ -1,3 +1,5 @@
+import { Token } from "./Phrase";
+
 export enum TokenType {
   Unknown = 0,
   Address = 1,
@@ -27,31 +29,15 @@ export class GrammarToken {
   private _t: TokenType;
   private _v: ValueType;
 
-  private map = {
-    "@": {
-      type: TokenType.Address
-    },
-    "#": {
-      type: TokenType.Address
-    },
-    "/": {
-      type: TokenType.Address
-    },
-    "&": {
-      type: TokenType.Address
-    },
-    "^": {
-      type: TokenType.Address
-    },
-    "!": {
-      type: TokenType.Address
-    },
-    ",": {
-      type: TokenType.Comment
-    },
-    "'": {
-      type: TokenType.Address
-    }
+  protected static typeMap: { [key: string]: TokenType } = {
+    "@": TokenType.Address,
+    "#": TokenType.Color,
+    "/": TokenType.Root,
+    "&": TokenType.Reference,
+    "^": TokenType.Parent,
+    "!": TokenType.Outward,
+    ",": TokenType.Comment,
+    "'": TokenType.Address
   }
 
   constructor(token: string) {
@@ -64,6 +50,20 @@ export class GrammarToken {
       type: TokenType.Unknown,
       value: ValueType.nul
     };
+
+    if (t.length > 0) {
+      const f = t.charAt(0);
+
+      retval.type = GrammarToken.typeMap[f] || TokenType.Unknown;
+      if (retval.type !== TokenType.Comment && retval.type !== TokenType.Unknown) {
+        // const r = (t.length > 1) ? t.substring(1) : "";
+        if (t.length > 1) {
+          const r = t.substring(1);
+
+        }
+      }
+
+    }
 
     return retval;
   }
